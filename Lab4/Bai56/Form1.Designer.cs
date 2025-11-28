c6e3c76d0ebbffac4e91a212bfcdd5c27b0ded7f
Lục Vĩnh Kiệt <24520903@gm.uit.edu.vn>
2025-11-28 20:02:15 +0700

    Hoàn thành bài 56

--- Lab4/Bai56/Form1.Designer.cs
+++ Lab4/Bai56/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tb_URL = new TextBox();
+            richTextBox1 = new RichTextBox();
+            tb_Username = new TextBox();
+            tb_Password = new TextBox();
+            btn_Login = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // tb_URL
+            // 
+            tb_URL.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_URL.Location = new Point(162, 27);
+            tb_URL.Name = "tb_URL";
+            tb_URL.Size = new Size(626, 35);
+            tb_URL.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 186);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(776, 252);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // tb_Username
+            // 
+            tb_Username.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_Username.Location = new Point(162, 80);
+            tb_Username.Name = "tb_Username";
+            tb_Username.Size = new Size(435, 35);
+            tb_Username.TabIndex = 2;
+            // 
+            // tb_Password
+            // 
+            tb_Password.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_Password.Location = new Point(162, 134);
+            tb_Password.Name = "tb_Password";
+            tb_Password.PasswordChar = '*';
+            tb_Password.Size = new Size(435, 35);
+            tb_Password.TabIndex = 3;
+            tb_Password.UseSystemPasswordChar = true;
+            // 
+            // btn_Login
+            // 
+            btn_Login.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Login.Location = new Point(639, 91);
+            btn_Login.Name = "btn_Login";
+            btn_Login.Size = new Size(124, 63);
+            btn_Login.TabIndex = 4;
+            btn_Login.Text = "Login";
+            btn_Login.UseVisualStyleBackColor = true;
+            btn_Login.Click += btn_Login_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(59, 27);
+            label1.TabIndex = 5;
+            label1.Text = "URL";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 27);
+            label2.TabIndex = 6;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 137);
+            label3.Name = "label3";
+            label3.Size = new Size(104, 27);
+            label3.TabIndex = 7;
+            label3.Text = "Password";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(btn_Login);
+            Controls.Add(tb_Password);
+            Controls.Add(tb_Username);
+            Controls.Add(richTextBox1);
+            Controls.Add(tb_URL);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tb_URL;
+        private RichTextBox richTextBox1;
+        private TextBox tb_Username;
+        private TextBox tb_Password;
+        private Button btn_Login;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
--- Lab4/Bai56/Form1.cs
+++ Lab4/Bai56/Form1.cs
@@ -1,3 +1,8 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
 namespace Bai56
 {
     public partial class Form1 : Form
@@ -5,6 +10,65 @@ namespace Bai56
         public Form1()
         {
             InitializeComponent();
+            tb_URL.Text = "https://nt106.uitiot.vn/auth/token";
+        }
+
+        private async void btn_Login_Click(object sender, EventArgs e)
+        {
+            string url = tb_URL.Text;
+            string username = tb_Username.Text;
+            string password = tb_Password.Text;
+            if (string.IsNullOrWhiteSpace(url) ||
+                string.IsNullOrWhiteSpace(username) ||
+                string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Vui long nhap URL, Username va Password!");
+                return;
+            }
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var content = new MultipartFormDataContent
+                    {
+                        { new StringContent(username), "username" },
+                        { new StringContent(password), "password" }
+                    };
+
+                    var response = await client.PostAsync(url, content);
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    var responseObject = JObject.Parse(responseString);
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        string detail = responseObject["detail"]?.ToString();
+                        richTextBox1.Text = "Đang nhap that bai!\nDetail: " + detail;
+                        return;
+                    }
+
+                    string tokenType = responseObject["token_type"]?.ToString();
+                    string accessToken = responseObject["access_token"]?.ToString();
+
+                    richTextBox1.Text =
+                        "Đang nhap thanh cong!\n\n" +
+                        $"Token Type: {tokenType}\n" +
+                        $"Access Token: {accessToken}\n\n";
+
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+
+                    var getUserUrl = "https://nt106.uitiot.vn/api/v1/user/me";
+                    var getUserResponse = await client.GetAsync(getUserUrl);
+                    var getUserString = await getUserResponse.Content.ReadAsStringAsync();
+
+                    richTextBox1.AppendText("User Info:\n" + getUserString);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loi ket noi: " + ex.Message);
+            }
         }
     }
 }
