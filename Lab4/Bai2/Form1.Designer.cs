7dfea0d40132e66e6d03e4c443741d7fcd904a17
Lục Vĩnh Kiệt <24520903@gm.uit.edu.vn>
2025-11-28 20:01:15 +0700

    Hoàn thành bài 2

--- Lab4/Bai2/Form1.Designer.cs
+++ Lab4/Bai2/Form1.Designer.cs
@@ -28,12 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tb_URL = new TextBox();
+            tb_FileURL = new TextBox();
+            rtb_HTML = new RichTextBox();
+            btn_Download = new Button();
+            SuspendLayout();
+            // 
+            // tb_URL
+            // 
+            tb_URL.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_URL.Location = new Point(22, 19);
+            tb_URL.Name = "tb_URL";
+            tb_URL.Size = new Size(587, 35);
+            tb_URL.TabIndex = 0;
+            // 
+            // tb_FileURL
+            // 
+            tb_FileURL.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_FileURL.Location = new Point(22, 78);
+            tb_FileURL.Name = "tb_FileURL";
+            tb_FileURL.Size = new Size(587, 35);
+            tb_FileURL.TabIndex = 2;
+            // 
+            // rtb_HTML
+            // 
+            rtb_HTML.Location = new Point(22, 140);
+            rtb_HTML.Name = "rtb_HTML";
+            rtb_HTML.ReadOnly = true;
+            rtb_HTML.ScrollBars = RichTextBoxScrollBars.Vertical;
+            rtb_HTML.Size = new Size(757, 298);
+            rtb_HTML.TabIndex = 3;
+            rtb_HTML.Text = "";
+            // 
+            // btn_Download
+            // 
+            btn_Download.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Download.Location = new Point(636, 12);
+            btn_Download.Name = "btn_Download";
+            btn_Download.Size = new Size(143, 46);
+            btn_Download.TabIndex = 4;
+            btn_Download.Text = "Download";
+            btn_Download.UseVisualStyleBackColor = true;
+            btn_Download.Click += btn_Download_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_Download);
+            Controls.Add(rtb_HTML);
+            Controls.Add(tb_FileURL);
+            Controls.Add(tb_URL);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox tb_URL;
+        private TextBox tb_FileURL;
+        private RichTextBox rtb_HTML;
+        private Button btn_Download;
     }
 }
--- Lab4/Bai2/Form1.cs
+++ Lab4/Bai2/Form1.cs
@@ -1,3 +1,8 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
 namespace Bai2
 {
     public partial class Form1 : Form
@@ -5,6 +10,32 @@ namespace Bai2
         public Form1()
         {
             InitializeComponent();
+        }
+        private void btn_Download_Click(object sender, EventArgs e)
+        {
+            string url = tb_URL.Text.Trim();
+            string savePath = tb_FileURL.Text.Trim();
+
+            if (string.IsNullOrEmpty(url) || string.IsNullOrEmpty(savePath))
+            {
+                MessageBox.Show("Vui lòng nhập URL và đường dẫn lưu file!");
+                return;
+            }
+            rtb_HTML.Text = "Loading ....";
+            try
+            {
+                using (WebClient web = new WebClient())
+                {
+                    web.DownloadFile(url, savePath);
+                    string htmlText = File.ReadAllText(savePath);
+                    rtb_HTML.Text = htmlText;
+                    MessageBox.Show("Download thành công!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi: " + ex.Message);
+            }
         }
     }
 }
