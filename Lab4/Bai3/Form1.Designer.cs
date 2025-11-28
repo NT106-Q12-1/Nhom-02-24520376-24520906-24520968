dfdb5edc6f1ce5b5e84b0e0a1f6d42cc79b40ca9
Lục Vĩnh Kiệt <24520903@gm.uit.edu.vn>
2025-11-28 20:01:47 +0700

    Hoàn thành bài 3

--- Lab4/Bai3/Form1.Designer.cs
+++ Lab4/Bai3/Form1.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            wv_Web = new Microsoft.Web.WebView2.WinForms.WebView2();
+            tb_URL = new TextBox();
+            btn_Load = new Button();
+            btn_DownloadFile = new Button();
+            btn_DownloadResource = new Button();
+            ((System.ComponentModel.ISupportInitialize)wv_Web).BeginInit();
+            SuspendLayout();
+            // 
+            // wv_Web
+            // 
+            wv_Web.AllowExternalDrop = true;
+            wv_Web.CreationProperties = null;
+            wv_Web.DefaultBackgroundColor = Color.White;
+            wv_Web.Location = new Point(12, 105);
+            wv_Web.Name = "wv_Web";
+            wv_Web.Size = new Size(776, 333);
+            wv_Web.TabIndex = 0;
+            wv_Web.ZoomFactor = 1D;
+            // 
+            // tb_URL
+            // 
+            tb_URL.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tb_URL.Location = new Point(20, 13);
+            tb_URL.Name = "tb_URL";
+            tb_URL.Size = new Size(629, 35);
+            tb_URL.TabIndex = 1;
+            // 
+            // btn_Load
+            // 
+            btn_Load.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Load.Location = new Point(655, 7);
+            btn_Load.Name = "btn_Load";
+            btn_Load.Size = new Size(133, 45);
+            btn_Load.TabIndex = 2;
+            btn_Load.Text = "Load";
+            btn_Load.UseVisualStyleBackColor = true;
+            btn_Load.Click += btn_Load_Click;
+            // 
+            // btn_DownloadFile
+            // 
+            btn_DownloadFile.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_DownloadFile.Location = new Point(89, 54);
+            btn_DownloadFile.Name = "btn_DownloadFile";
+            btn_DownloadFile.Size = new Size(252, 45);
+            btn_DownloadFile.TabIndex = 3;
+            btn_DownloadFile.Text = "Download File";
+            btn_DownloadFile.UseVisualStyleBackColor = true;
+            btn_DownloadFile.Click += btn_DownloadFile_Click;
+            // 
+            // btn_DownloadResource
+            // 
+            btn_DownloadResource.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_DownloadResource.Location = new Point(444, 54);
+            btn_DownloadResource.Name = "btn_DownloadResource";
+            btn_DownloadResource.Size = new Size(252, 45);
+            btn_DownloadResource.TabIndex = 4;
+            btn_DownloadResource.Text = "Download Resource";
+            btn_DownloadResource.UseVisualStyleBackColor = true;
+            btn_DownloadResource.Click += btn_DownloadResource_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_DownloadResource);
+            Controls.Add(btn_DownloadFile);
+            Controls.Add(btn_Load);
+            Controls.Add(tb_URL);
+            Controls.Add(wv_Web);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)wv_Web).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 wv_Web;
+        private TextBox tb_URL;
+        private Button btn_Load;
+        private Button btn_DownloadFile;
+        private Button btn_DownloadResource;
     }
 }
--- Lab4/Bai3/Form1.cs
+++ Lab4/Bai3/Form1.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Security.Policy;
+using Microsoft.Web.WebView2.Core;
+
 namespace Bai3
 {
     public partial class Form1 : Form
@@ -5,6 +9,99 @@ namespace Bai3
         public Form1()
         {
             InitializeComponent();
+            wv_Web.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            await wv_Web.EnsureCoreWebView2Async(null);
+            wv_Web.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+            await wv_Web.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.location.href);");
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            tb_URL.Text = uri;
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                wv_Web.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        private void btn_Load_Click(object sender, EventArgs e)
+        {
+            if (wv_Web != null && wv_Web.CoreWebView2 != null)
+            {
+                wv_Web.CoreWebView2.Navigate(tb_URL.Text);
+            }
+        }
+
+        private async void btn_DownloadFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = tb_URL.Text;
+                HttpClient client = new HttpClient();
+                string html = await client.GetStringAsync(url);
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = "HTML File|*.html";
+                dialog.FileName = "download.html";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(dialog.FileName, html);
+                    MessageBox.Show("Tai HTML thanh cong!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loi tai HTML: " + ex.Message);
+            }
+        }
+
+        private async void btn_DownloadResource_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = tb_URL.Text;
+                using (HttpClient client = new HttpClient())
+                {
+                    string html = await client.GetStringAsync(url);
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(html);
+                    var nodes = doc.DocumentNode.SelectNodes("//img[@src]");
+                    if (nodes == null)
+                    {
+                        MessageBox.Show("Khong tim thay anh!");
+                        return;
+                    }
+                    using FolderBrowserDialog fbd = new FolderBrowserDialog();
+                    if (fbd.ShowDialog() != DialogResult.OK) return;
+                    string folder = fbd.SelectedPath;
+                    foreach (var node in nodes)
+                    {
+                        string src = node.GetAttributeValue("src", "");
+                        Uri baseUri = new Uri(url);
+                        Uri fullUri = new Uri(baseUri, src);
+                        byte[] data = await client.GetByteArrayAsync(fullUri);
+                        string fileName = Path.GetFileName(fullUri.LocalPath);
+                        string savePath = Path.Combine(folder, fileName);
+                        File.WriteAllBytes(savePath, data);
+                    }
+                    MessageBox.Show("Tai anh xong!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Loi Download Resource: " + ex.Message);
+            }
         }
     }
 }
